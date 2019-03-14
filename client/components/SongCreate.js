7dffe91a47de850996e13a6b3bbe54a8e2a36a58
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-14 09:42:52 -0600

    Wired up SongCreate onSubmit

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,15 +1,23 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { Component } from 'react';
 
 export default class SongCreate extends Component {
   state = { title: '' };
 
+  onSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
-    const { title } = this.state;
+    const { title, id } = this.state;
 
     return (
       <div>
         <h3>Create a new song...</h3>
-        <form>{title}</form>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title:</label>
+          <input onChange={event => this.setState({ title: event.target.value })} value={title} />
+        </form>
       </div>
     );
   }

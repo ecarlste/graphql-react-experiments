10bcf2ee037098e6452d17d6cf1899c77dfa23a8
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-15 00:59:37 -0600

    Set up some scaffolding for the nav bar

--- auth/client/components/Header.js
+++ auth/client/components/Header.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router-dom';
+import query from '../queries/CurrentUser';
 
-const Header = () => {
-  return <div>Header</div>;
-};
+class Header extends Component {
+  renderButtons() {
+    const { loading, user } = this.props.data;
 
-export default Header;
+    if (loading) {
+      return <div />;
+    }
+
+    if (user) {
+      return <div>Logout</div>;
+    }
+
+    return (
+      <Fragment>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+      </Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <nav>
+        <Link to="/" className="brand-logo left">
+          Home
+        </Link>
+        <ul className="right">
+          <div className="nav-wrapper">{this.renderButtons()}</div>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default graphql(query)(Header);

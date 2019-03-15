0fe3257cd3d009b87083215689adfcdfb2aefbe5
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-14 23:08:19 -0600

    Add in memory cache for apollo by id

--- auth/client/index.js
+++ auth/client/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient, { InMemoryCache } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { Router, hashHistory, Route, IndexRoute } from "react-router";
+
+const cache = new InMemoryCache({
+  dataIdFromObject: object => object.id || null
+});
+
+const client = new ApolloClient({ cache });
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <div>Auth Starter</div>
+    </ApolloProvider>
   );
 };
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(<Root />, document.querySelector("#root"));
